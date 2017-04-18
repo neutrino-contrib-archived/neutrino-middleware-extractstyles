b7d0172284775a3c611720824afd54b533ea2cec
Jarid Margolin <jaridmargolin@gmail.com>
2017-04-18 12:26:28 -0400

    Make sure removing the existing style rule is the first thing we do (temporary).

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
  * -------------------------------------------------------------------------- */
 
 module.exports = (neutrino, options = {}) => {
+  // We want to start from a clean slate. This removes any existing "style"
+  // rule that may exist (present if using the default neutrino-preset-web).
+  neutrino.config.module.rules.delete('style')
+
   const { loaderOptions = {}, pluginOptions = {} } = options
   const styleRule = neutrino.config.module
     .rule('style')
@@ -21,10 +25,6 @@ module.exports = (neutrino, options = {}) => {
     fallback: 'style-loader',
     use: 'css-loader'
   }, loaderOptions))
-
-  // We want to start from a clean slate. This removes any existing "style"
-  // rule that may exist (present if using the default neutrino-preset-web).
-  neutrino.config.module.rules.delete('style')
 
   loaders.forEach(({ loader, options }) => styleRule.use(loader)
     .loader(loader)
